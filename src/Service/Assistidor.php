f6d4138b29e74f43d44b517040d4dcc7e0680f21
Mateusdeli <mateus_forlevesi@hotmail.com>
2021-01-20 14:54:22 -0300

    Implementacao do princípio de Segregação de Interfaces

--- src/Service/Assistidor.php
+++ src/Service/Assistidor.php
@@ -3,13 +3,14 @@
 namespace Alura\Solid\Service;
 
 use Alura\Solid\Model\AluraMais;
+use Alura\Solid\Model\Assistivel;
 use Alura\Solid\Model\Curso;
 use Alura\Solid\Model\Pontuavel;
 
 class Assistidor
 {
-    public function assistir(Pontuavel $pontuavel)
+    public function assistir(Assistivel $assistivel)
     {
-        $pontuavel->assistir();
+        $assistivel->assistir();
     }
 }
